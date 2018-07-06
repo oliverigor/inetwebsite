c28637e44e81dd859ad95d74d03af49a11a698fc
Igor Martins <m.oliverunb@gmail.com>
2018-07-06 10:51:14 -0300

    Adicionando bibliotecas

--- src/components/shoparea.js
+++ src/components/shoparea.js
@@ -1,61 +1,98 @@
-import React, { Component } from 'react';
-import '../index.css';
-import PosiPc from '../pc1.png';
+import React, { Component } from "react";
+import "../index.css";
+import PosiPc from "../pc1.png";
 
 class ShopArea extends Component {
   render() {
     return (
+      <div className="div-style">
+        <div className="temcard">
+          <div className="card">
+            <img src={PosiPc} className="img-fit" />
+            <hr />
+            <p>
+              Fundada em Novembro de 2014 e com o objetivo de disseminar ao
+              público experiências simples e inovadoras dos jogos, a Mad Pixel
+              se estabeleceu no mercado após vencer a game jam na JAM NERD
+              FESTIVAL.
+            </p>
+            <a>Comprar</a>
+          </div>
+          <div className="card">
+            <h3>Jogos</h3>
+            <p>
+              Fundada em Novembro de 2014 e com o objetivo de disseminar ao
+              público experiências simples e inovadoras dos jogos, a Mad Pixel
+              se estabeleceu no mercado após vencer a game jam na JAM NERD
+              FESTIVAL.
+            </p>
+            <a>Comprar</a>
+          </div>
 
-      <div  className="div-style">
-		<div className="temcard">
-			<div className="card">
-				<img src={PosiPc} className="img-fit" />
-				<hr/>
-				<p>Fundada em Novembro de 2014 e com o objetivo de disseminar ao público experiências simples e inovadoras dos jogos, a Mad Pixel se estabeleceu no mercado após vencer a game jam na JAM NERD FESTIVAL.</p>
-				<a>Comprar</a>
-			</div>
-			<div className="card">
-				<h3>Jogos</h3>
-				<p>Fundada em Novembro de 2014 e com o objetivo de disseminar ao público experiências simples e inovadoras dos jogos, a Mad Pixel se estabeleceu no mercado após vencer a game jam na JAM NERD FESTIVAL.</p>
-				<a>Comprar</a>
-			</div>
-
-			<div class="card">
-				<h3>Jogos</h3>
-				<p>Fundada em Novembro de 2014 e com o objetivo de disseminar ao público experiências simples e inovadoras dos jogos, a Mad Pixel se estabeleceu no mercado após vencer a game jam na JAM NERD FESTIVAL.</p>
-				<a>Comprar</a>
-			</div>
-      <div class="card">
-				<h3>Jogos</h3>
-				<p>Fundada em Novembro de 2014 e com o objetivo de disseminar ao público experiências simples e inovadoras dos jogos, a Mad Pixel se estabeleceu no mercado após vencer a game jam na JAM NERD FESTIVAL.</p>
-				<a>Comprar</a>
-			</div>
-      <div class="card">
-				<h3>Jogos</h3>
-				<p>Fundada em Novembro de 2014 e com o objetivo de disseminar ao público experiências simples e inovadoras dos jogos, a Mad Pixel se estabeleceu no mercado após vencer a game jam na JAM NERD FESTIVAL.</p>
-				<a>Comprar</a>
-			</div>
-      <div class="card">
-				<h3>Jogos</h3>
-				<p>Fundada em Novembro de 2014 e com o objetivo de disseminar ao público experiências simples e inovadoras dos jogos, a Mad Pixel se estabeleceu no mercado após vencer a game jam na JAM NERD FESTIVAL.</p>
-				<a>Comprar</a>
-			</div>
-      <div class="card">
-				<h3>Jogos</h3>
-				<p>Fundada em Novembro de 2014 e com o objetivo de disseminar ao público experiências simples e inovadoras dos jogos, a Mad Pixel se estabeleceu no mercado após vencer a game jam na JAM NERD FESTIVAL.</p>
-				<a>Comprar</a>
-			</div>
-      <div class="card">
-				<h3>Jogos</h3>
-				<p>Fundada em Novembro de 2014 e com o objetivo de disseminar ao público experiências simples e inovadoras dos jogos, a Mad Pixel se estabeleceu no mercado após vencer a game jam na JAM NERD FESTIVAL.</p>
-				<a>Comprar</a>
-			</div>
-		</div>
-
-
+          <div class="card">
+            <h3>Jogos</h3>
+            <p>
+              Fundada em Novembro de 2014 e com o objetivo de disseminar ao
+              público experiências simples e inovadoras dos jogos, a Mad Pixel
+              se estabeleceu no mercado após vencer a game jam na JAM NERD
+              FESTIVAL.
+            </p>
+            <a>Comprar</a>
+          </div>
+          <div class="card">
+            <h3>Jogos</h3>
+            <p>
+              Fundada em Novembro de 2014 e com o objetivo de disseminar ao
+              público experiências simples e inovadoras dos jogos, a Mad Pixel
+              se estabeleceu no mercado após vencer a game jam na JAM NERD
+              FESTIVAL.
+            </p>
+            <a>Comprar</a>
+          </div>
+          <div class="card">
+            <h3>Jogos</h3>
+            <p>
+              Fundada em Novembro de 2014 e com o objetivo de disseminar ao
+              público experiências simples e inovadoras dos jogos, a Mad Pixel
+              se estabeleceu no mercado após vencer a game jam na JAM NERD
+              FESTIVAL.
+            </p>
+            <a>Comprar</a>
+          </div>
+          <div class="card">
+            <h3>Jogos</h3>
+            <p>
+              Fundada em Novembro de 2014 e com o objetivo de disseminar ao
+              público experiências simples e inovadoras dos jogos, a Mad Pixel
+              se estabeleceu no mercado após vencer a game jam na JAM NERD
+              FESTIVAL.
+            </p>
+            <a>Comprar</a>
+          </div>
+          <div class="card">
+            <h3>Jogos</h3>
+            <p>
+              Fundada em Novembro de 2014 e com o objetivo de disseminar ao
+              público experiências simples e inovadoras dos jogos, a Mad Pixel
+              se estabeleceu no mercado após vencer a game jam na JAM NERD
+              FESTIVAL.
+            </p>
+            <a>Comprar</a>
+          </div>
+          <div class="card">
+            <h3>Jogos</h3>
+            <p>
+              Fundada em Novembro de 2014 e com o objetivo de disseminar ao
+              público experiências simples e inovadoras dos jogos, a Mad Pixel
+              se estabeleceu no mercado após vencer a game jam na JAM NERD
+              FESTIVAL.
+            </p>
+            <a>Comprar</a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export  default ShopArea;
+export default ShopArea;
