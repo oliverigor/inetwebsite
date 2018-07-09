ab90ff35a1c86f9f793ee5d691663aa285ae5a1c
Igor Martins <m.oliverunb@gmail.com>
2018-07-09 15:14:39 -0300

    adicionando logo nova, e colocando o slider e a modal, sem funcionar perfeitamente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import Navbar from './components/navbar.js';
-import './App.css';
-import ShopArea from './components/shoparea.js';
-import InetLogo from './inet-logo.svg';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Navbar from "./components/navbar.js";
+import "./App.css";
+import ShopArea from "./components/shoparea.js";
+import InetLogo from "./inetlogo.svg";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <header className="App-header">
-          <Navbar/>
-          <img src={InetLogo}/>
-          
+        <header className="App-header">
+          <Navbar />
+          <img src={InetLogo} />
         </header>
 
-      <section className="section">
-        <ShopArea/>
-      </section>
-
-
+        <section className="section">
+          <ShopArea />
+        </section>
       </div>
     );
   }
--- src/components/shoparea.js
+++ src/components/shoparea.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 import "../index.css";
 import PosiPc from "../pc1.png";
+import Modal from "react-responsive-modal";
+
+import Carousel from "nuka-carousel";
 
 class ShopArea extends Component {
+  state = {
+    open: false
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { open } = this.state;
     return (
       <div className="div-style">
         <div className="temcard">
           <div className="card">
-            <img src={PosiPc} className="img-fit" />
+            <div>
+              <Carousel>
+                <img src={PosiPc} className="img-fit" />
+                <img src={PosiPc} className="img-fit" />
+                <img src={PosiPc} className="img-fit" />
+              </Carousel>
+            </div>
             <hr />
             <p>
               Fundada em Novembro de 2014 e com o objetivo de disseminar ao
@@ -16,7 +38,17 @@ class ShopArea extends Component {
               se estabeleceu no mercado após vencer a game jam na JAM NERD
               FESTIVAL.
             </p>
-            <a>Comprar</a>
+            <a onClick={this.onOpenModal}>Comprar</a>
+            <Modal open={open} onClose={this.onCloseModal} center>
+              <p>VEEEEI EHUAHAUEHUAHUAEH</p>
+              <div style={{ width: "700px", height: "500px" }}>
+                <Carousel>
+                  <img src={PosiPc} style={{ width: "50%", height: "50%" }} />
+                  <img src={PosiPc} style={{ width: "50%", height: "50%" }} />
+                  <img src={PosiPc} style={{ width: "50%", height: "50%" }} />
+                </Carousel>
+              </div>
+            </Modal>
           </div>
           <div className="card">
             <h3>Jogos</h3>
