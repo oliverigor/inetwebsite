b0870e4a4858ac5cc1236c9123209e096a633e54
Igor Martins <m.oliverunb@gmail.com>
2018-07-23 16:52:51 -0300

    imagem da positivo

--- src/components/shoparea.js
+++ src/components/shoparea.js
@@ -48,73 +48,77 @@ class ShopArea extends Component {
         <div className="temcard">
           {data.map(pc => {
             return (
-              <div className="card">
-                <div>
-                  <Carousel>
-                    <Carousel.Item>
-                      <img src={pc.pic1} />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img src={pc.pic2} />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img src={pc.pic3} />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img src={pc.pic4} />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                      <img src={pc.pic5} />
-                    </Carousel.Item>
-                  </Carousel>
-                </div>
-                <hr />
-                <p>{pc.modelo}</p>
-                <p />
-                <a onClick={this.openModal}>Comprar</a>
-                <Modal
-                  isOpen={this.state.modalIsOpen}
-                  onAfterOpen={this.afterOpenModal}
-                  onRequestClose={this.closeModal}
-                  style={customStyles}
-                  contentLabel="Example Modal"
-                >
-                  <p>VEEEEI EHUAHAUEHUAHUAEH</p>
-                  <div className="card">
-                    <div>
-                      <Carousel>
-                        <Carousel.Item>
-                          <img src={pc.pic1} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                          <img src={pc.pic2} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                          <img src={pc.pic3} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                          <img src={pc.pic4} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                          <img src={pc.pic5} />
-                        </Carousel.Item>
-                      </Carousel>
-                    </div>
-                    <hr />
-                    <p>
-                      {/* 
-                        <a href="https://imgbb.com/"><img src="https://image.ibb.co/dJ21EJ/D365_04.jpg" alt="D365_04" border="0"></a>
-                        <a href="https://imgbb.com/"><img src="https://image.ibb.co/m2riTd/D365_03_1.jpg" alt="D365_03_1" border="0"></a>
-                          <a href="https://imgbb.com/"><img src="https://image.ibb.co/hB88uJ/D365_03.jpg" alt="D365_03" border="0"></a>
-                        <a href="https://imgbb.com/"><img src="https://image.ibb.co/dhriTd/D635_02.jpg" alt="D635_02" border="0"></a>
-                        <a href="https://imgbb.com/"><img src="https://image.ibb.co/bYAMEJ/D365.jpg" alt="D365" border="0"></a>
-                        <a href="https://imgbb.com/"><img src="https://image.ibb.co/jHuRgy/T50_angulo2.jpg" alt="T50_angulo2" border="0"></a>
-                        <a href="https://imgbb.com/"><img src="https://image.ibb.co/cq9sMy/master_C1002.jpg" alt="master_C1002" border="0"></a>
-                      <a href="https://imgbb.com/"><img src="https://image.ibb.co/g6KEZJ/pc1.png" alt="pc1" border="0"></a> */}
-                    </p>
-                    <a onClick={this.onOpenModal}>Comprar</a>
+              <div>
+                <div className="card">
+                  <div>
+                    <Carousel>
+                      <Carousel.Item>
+                        <img src={pc.pic1} />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img src={pc.pic2} />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img src={pc.pic3} />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img src={pc.pic4} />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img src={pc.pic5} />
+                      </Carousel.Item>
+                    </Carousel>
                   </div>
-                </Modal>
+                  <hr />
+                  <p>{pc.modelo}</p>
+                  <p />
+                  <a onClick={this.openModal}>Comprar</a>
+                </div>
+                <div>
+                  <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    onRequestClose={this.closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                  >
+                    <p>VEEEEI EHUAHAUEHUAHUAEH</p>
+                    <div className="card">
+                      <div>
+                        <Carousel>
+                          <Carousel.Item>
+                            <img src={pc.pic1} />
+                          </Carousel.Item>
+                          <Carousel.Item>
+                            <img src={pc.pic2} />
+                          </Carousel.Item>
+                          <Carousel.Item>
+                            <img src={pc.pic3} />
+                          </Carousel.Item>
+                          <Carousel.Item>
+                            <img src={pc.pic4} />
+                          </Carousel.Item>
+                          <Carousel.Item>
+                            <img src={pc.pic5} />
+                          </Carousel.Item>
+                        </Carousel>
+                      </div>
+                      <hr />
+                      <p>
+                        {/*
+                          <a href="https://imgbb.com/"><img src="https://image.ibb.co/dJ21EJ/D365_04.jpg" alt="D365_04" border="0"></a>
+                          <a href="https://imgbb.com/"><img src="https://image.ibb.co/m2riTd/D365_03_1.jpg" alt="D365_03_1" border="0"></a>
+                            <a href="https://imgbb.com/"><img src="https://image.ibb.co/hB88uJ/D365_03.jpg" alt="D365_03" border="0"></a>
+                          <a href="https://imgbb.com/"><img src="https://image.ibb.co/dhriTd/D635_02.jpg" alt="D635_02" border="0"></a>
+                          <a href="https://imgbb.com/"><img src="https://image.ibb.co/bYAMEJ/D365.jpg" alt="D365" border="0"></a>
+                          <a href="https://imgbb.com/"><img src="https://image.ibb.co/jHuRgy/T50_angulo2.jpg" alt="T50_angulo2" border="0"></a>
+                          <a href="https://imgbb.com/"><img src="https://image.ibb.co/cq9sMy/master_C1002.jpg" alt="master_C1002" border="0"></a>
+                        <a href="https://imgbb.com/"><img src="https://image.ibb.co/g6KEZJ/pc1.png" alt="pc1" border="0"></a> */}
+                      </p>
+                      <a onClick={this.onOpenModal}>Comprar</a>
+                    </div>
+                  </Modal>
+                </div>
               </div>
             );
           })}
